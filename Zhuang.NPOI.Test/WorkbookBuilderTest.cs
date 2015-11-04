55319df28ae7a18afe97d5edf74e391c7f3d2db3
zhuangweibin <binweizhuang@hotmail.com>
2015-11-05 00:15:49 +0800

    add WorkbookBuilderTest

---
+++ Zhuang.NPOI.Test/WorkbookBuilderTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Zhuang.NPOI.Excel;
+using System.Data;
+using Zhuang.Data;
+
+namespace Zhuang.NPOI.Test
+{
+
+    [TestClass]
+    public class WorkbookBuilderTest
+    {
+        DbAccessor _dba = DbAccessor.Get();
+
+        [TestMethod]
+        public void HSSFWorkbookBuilderTest()
+        {
+            HSSFWorkbookBuilder builder = new HSSFWorkbookBuilder();
+            DataTable dt = _dba.QueryDataTable("select top 10 * from sys_product");
+            builder.SetDataTable(dt)
+                .SetColumnCaption("产品名称", "ProductName").SetColumnWidth(20)
+                .Build().Save(@"C:\npoitest.xls");
+        }
+    }
+}
